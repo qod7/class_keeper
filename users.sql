0474115731a694951e3e53809292f989c4c971c1
diwasblack <diwasblack@gmail.com>
2014-06-27 23:42:43 +0545

    Database

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 27, 2014 at 05:36 PM
+-- Generation Time: Jun 27, 2014 at 11:41 PM
 -- Server version: 5.5.37-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.2
 
@@ -28,12 +28,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role` int(11) NOT NULL,
+  `role` tinyint(4) NOT NULL,
+  `school_id` int(11) NOT NULL DEFAULT '0',
+  `username` text COLLATE utf8_unicode_ci NOT NULL,
+  `password` text COLLATE utf8_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `remember_token` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role`, `school_id`, `username`, `password`, `created_at`, `updated_at`, `remember_token`) VALUES
+(1, 0, 0, 'admin', '$2y$10$6NyXvFeSXrJ7tChKusi9oeowTJVSSOHclae3gSx65QeMAAjTjLEyC', '2014-06-27 17:24:18', '2014-06-27 17:56:33', 'zxwbc6HqUwHfseeDjz5VxdRb6lTGseiB4hyDBASjoYYejgooL3NxQXvwyN9X');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
