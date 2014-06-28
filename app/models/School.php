4d3be7eef1eeee3ee808e281fd9cc30e1bf54fa7
shashwatblack <shashwat.black@gmail.com>
2014-06-28 10:30:42 +0545

    Added school model

---
+++ app/models/School.php
@@ -0,0 +1,15 @@
+<?php
+
+class School extends Eloquent {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'school';
+
+    public function getHeadMaster()
+    {
+    }
+}
