b70ed98f0803b96bba0a1315bca43ebfdaa6305e
diwasblack <diwasblack@gmail.com>
2014-06-28 11:47:02 +0545

    Form submission

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -16,4 +16,24 @@ class AdminController extends BaseController {
 	{
 		return View::make('addschool');
 	}
+
+	public function saveschool()
+	{
+		$input = Input::all();
+		$validator = Validator::make($input,
+			array(
+				'id' => 'required|unique:schools,name',
+				'name' => 'required|min:4'
+				));
+
+		if (!$validator->fails())
+		{
+			$school= new School();
+			$school->id=$input['id'];
+			$school->name=$input['name'];
+			$school->totalclasses=$input['totalclasses'];
+			$school->save();
+		}
+
+	}
 }
--- app/views/addschool.blade.php
+++ app/views/addschool.blade.php
@@ -5,11 +5,52 @@
 @endsection
 
 @section('content')
-	<div class="col-xs-12 col-sm-4">
+	<div class="col-xs-8 col-xs-offset-2">
 		<div class="box">
 			<div class="box-header">
+				<div class="box-name">
+					<span>Add School</span>
+				</div>
+				<div class="box-icons">
+					<a class="expand-link">
+						<i class="fa fa-expand"></i>
+					</a>
+				</div>
+				<div class="no-move"></div>
 			</div>
 			<div class="box-content">
+				<form id="defaultForm" method="post" action="{{ URL::route('saveschool') }}" class="form-horizontal">
+					<fieldset>
+						<legend>Fill the following</legend>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Id</label>
+							<div class="col-sm-8">
+								<input type="text" class="form-control" name="id" />
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Name</label>
+							<div class="col-sm-8">
+								<input type="text" class="form-control" name="name" />
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Total Classes</label>
+							<div class="col-sm-8">
+								<input type="text" class="form-control" name="totalclasses" />
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-sm-offset-3 col-sm-8">
+								<button class="btn btn-primary btn-label-left" type="submit">Submit</button>
+							</div>
+						</div>
+
+					</fieldset>
+				</form>
 			</div>
 		</div>
 	</div>
